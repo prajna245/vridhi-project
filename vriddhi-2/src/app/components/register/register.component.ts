b0bf41f4ef47696e0be46889826cc55208c199b8
Prajna12345 <96471059+Prajna12345@users.noreply.github.com>
2024-05-28 12:50:46 +0530

    first commi6

---
+++ vriddhi-2/src/app/components/register/register.component.ts
@@ -0,0 +1,32 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      otp: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+    }
+  }
+
+  requestOTP() {
+  }
+}
---
+++ vriddhi-2/src/app/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+  createUser(user: any) {
+    return this.http.post<any>('http://localhost:3007/users', user); 
+  }
+}
+
+
